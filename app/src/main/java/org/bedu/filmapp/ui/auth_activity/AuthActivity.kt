268afe1e65a722393fff0751944adebbce883be2
Jhovany Lopez <iscjhovany@gmail.com>
2023-05-12 22:44:48 -0500

    complete design screen auth (auth_home, auth_login, auth_signup)

---
+++ app/src/main/java/org/bedu/filmapp/ui/auth_activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package org.bedu.filmapp.ui.auth_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.bedu.filmapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
